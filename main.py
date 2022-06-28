b6b64a97926859c29f3bfeedc36da307714a62f0
bhubany <yadav.bhuban.by@gmail.com>
2022-06-28 15:30:07 +0545

    Code cleaning done

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
 
-
 # difining Different data types
 types={'integer' : "<class 'int'>",
         'string' : "<class 'str'>", 
@@ -9,6 +8,7 @@ types={'integer' : "<class 'int'>",
         'dictionary': "<class 'dict'>",
         'tuple': "<class 'tuple'>",
         }
+
 errors={}
 
 def validator(dict={}, validation_rules={}):
@@ -16,41 +16,30 @@ def validator(dict={}, validation_rules={}):
     for var in validation_rules:
         err={}
         for v in validation_rules[var]:
-            if (v == 'type'):
-                if(f"{type(dict[var])}" != types[validation_rules[var][v]]):
-                    err[v] = "Type Doesnot Matched (Validation: {}, obtained: {})".format(types[validation_rules[var][v]], type(dict[var]))
+            if (((v == 'type') and (str(type(dict[var])) != types[validation_rules[var][v]]))):
+                err[v] = "Type Doesnot Matched (Validation: {}, obtained: {})".format(types[validation_rules[var][v]], type(dict[var]))
 
-            elif(v == 'minLength'):
-                if(len(dict[var]) < validation_rules[var][v]):
-                    err[v] = "Length less than minimum (minimum Length: {}, obtained length: {})".format(validation_rules[var][v], len(dict[var]))
+            elif(v == 'minLength' and (len(dict[var]) < validation_rules[var][v])):
+                err[v] = "Length less than minimum (minimum Length: {}, obtained length: {})".format(validation_rules[var][v], len(dict[var]))
 
-            elif(v == 'maxLength'):
-                if(len(dict[var]) > validation_rules[var][v]):
-                    err[v] = "Length exceed than maximum (maximum Length: {}, obtained length: {})".format(validation_rules[var][v], len(dict[var]))
+            elif(v == 'maxLength' and (len(dict[var]) > validation_rules[var][v])):
+                err[v] = "Length exceed than maximum (maximum Length: {}, obtained length: {})".format(validation_rules[var][v], len(dict[var]))
 
-            elif(v == 'required'):
-                if((validation_rules[var][v] == True) and not(dict[var])):
-                    err[v] = "Required Field is set to {} for {}.".format(validation_rules[var][v], v)
+            elif(v == 'required' and ((validation_rules[var][v] == True) and not(dict[var]))):
+                err[v] = "Required Field is set to {} for {}.".format(validation_rules[var][v], v)
                   
-            elif (v == 'minValue'):
-                if(dict[var] < validation_rules[var][v]):
-                    err[v] = "Minimum Value error (minimum value: {}, obtained value: {})".format(validation_rules[var][v], dict[var])
+            elif (v == 'minValue' and (dict[var] < validation_rules[var][v])):
+                err[v] = "Minimum Value error (minimum value: {}, obtained value: {})".format(validation_rules[var][v], dict[var])
 
-            elif(v == 'maxValue'):
-                if(dict[var] > validation_rules[var][v]):
-                    err[v] = "Maximum value error (Maximum Length: {}, obtained length: {})".format(validation_rules[var][v], dict[var])
+            elif(v == 'maxValue' and (dict[var] > validation_rules[var][v])):
+                err[v] = "Maximum value error (Maximum Length: {}, obtained length: {})".format(validation_rules[var][v], dict[var])
 
-            elif(v == 'regex'):
-                patterns = re.compile(validation_rules[var][v], re.I)
-                if(not re.fullmatch(patterns, dict[var])):
-                    err[v] = "Regex matching error occurs (Set patterns: {} obtained value: {})".format(validation_rules[var][v], dict[var])
+            elif(v == 'regex' and (not re.fullmatch(re.compile(validation_rules[var][v]), dict[var]))):
+                err[v] = "Regex matching error occurs (Set patterns: {} obtained value: {})".format(validation_rules[var][v], dict[var])
 
-            elif(v == 'enumerate'):
-                if(dict[var] not in validation_rules[var][v]):
-                    err[v] = "Out of defined value (Set values: {}, obtained value: {})".format(validation_rules[var][v], dict[var])
+            elif(v == 'enumerate' and (dict[var] not in validation_rules[var][v])):
+                err[v] = "Out of defined value (Set values: {}, obtained value: {})".format(validation_rules[var][v], dict[var])
 
-            else:
-                print("Validator not Found ")
         if(err):
             errors[var]=err
     
@@ -68,8 +57,8 @@ validation_rules={
         'maxLength': 500
     },
     'address':{
-        'type': 'integer',
-        'minLength': 100,
+        'type': 'string',
+        'minLength': 10,
         'maxLength':50,
         'required':True,
     },
@@ -87,7 +76,7 @@ validation_rules={
 }
 
 
-dict ={'name': 'Bhuban Yadav', 'age':24, 'day':'noday' ,'birthYear':1998, 'birthMonth': 4, 'birthDay': 18, 'address': 'Dhapakhel-23, Lalitpur Nepal','email':'yadav.bhuban.by@gmail.com'}
+dict ={'name': 'Bhuban Yadav', 'age':24, 'day':'monday' ,'birthYear':1998, 'birthMonth': 4, 'birthDay': 18, 'address': 'Dhapakhel-23, Lalitpur Nepal','email':'yadav.bhuban.by@gmail.com'}
 
 
 
