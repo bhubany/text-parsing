e2dade9bb31b5e0d40bf3e8126e1a4d7011f008d
bhubany <yadav.bhuban.by@gmail.com>
2022-06-28 15:07:49 +0545

    Generic data dict validator compleated

--- main.py
+++ main.py
@@ -1,37 +1,96 @@
-import json
-import requests
-
-list2=[]
-
-def send_request(page=1, keyword="कोरोना", list2=[]):
-    while(page <= 3):
-        url ="https://bg.annapurnapost.com/api/search?title="+str(keyword)+"&page="+str(page)    
-
-        response = requests.get(url)
-        if(response.status_code == 200):         
-            res = response.json()
-            res_data=res['data']['items']                      
-            list2.append(res_data)
-            page+=1
-        else:
-            print("Error Occurs with Status Code: ",response.status_code )
-            break
-
-    if(len(list2) != 0):
-        try:
-            with open("jsonFile.json", "w", encoding="utf-8") as file:
-                json.dump({'current_page':(page), 'articles':list2}, file, indent=2)
-        except Exception as err:
-            print("Error Occurs creating Json File : ",err)
-        finally:
-            file.close()
+import re
 
 
-query_str = input("Enter Query String: ") # Taking Keywords
-try:
-    file= open('jsonFile.json', 'r', encoding="utf-8")
-    data = json.load(file)
-    print("Up to page = {} Has been fetched (limit is set to 3).".format(data['current_page'] - 1))
-    send_request(page=data['current_page'], keyword=query_str, list2=data['articles'])
-except:
-    send_request(keyword=query_str)
+# difining Different data types
+types={'integer' : "<class 'int'>",
+        'string' : "<class 'str'>", 
+        'boolean' : "<class 'bool'>", 
+        'list' : "<class 'bool'>",
+        'dictionary': "<class 'dict'>",
+        'tuple': "<class 'tuple'>",
+        }
+errors={}
+
+def validator(dict={}, validation_rules={}):
+    global types, errors
+    for var in validation_rules:
+        err={}
+        for v in validation_rules[var]:
+            if (v == 'type'):
+                if(f"{type(dict[var])}" != types[validation_rules[var][v]]):
+                    err[v] = "Type Doesnot Matched (Validation: {}, obtained: {})".format(types[validation_rules[var][v]], type(dict[var]))
+
+            elif(v == 'minLength'):
+                if(len(dict[var]) < validation_rules[var][v]):
+                    err[v] = "Length less than minimum (minimum Length: {}, obtained length: {})".format(validation_rules[var][v], len(dict[var]))
+
+            elif(v == 'maxLength'):
+                if(len(dict[var]) > validation_rules[var][v]):
+                    err[v] = "Length exceed than maximum (maximum Length: {}, obtained length: {})".format(validation_rules[var][v], len(dict[var]))
+
+            elif(v == 'required'):
+                if((validation_rules[var][v] == True) and not(dict[var])):
+                    err[v] = "Required Field is set to {} for {}.".format(validation_rules[var][v], v)
+                  
+            elif (v == 'minValue'):
+                if(dict[var] < validation_rules[var][v]):
+                    err[v] = "Minimum Value error (minimum value: {}, obtained value: {})".format(validation_rules[var][v], dict[var])
+
+            elif(v == 'maxValue'):
+                if(dict[var] > validation_rules[var][v]):
+                    err[v] = "Maximum value error (Maximum Length: {}, obtained length: {})".format(validation_rules[var][v], dict[var])
+
+            elif(v == 'regex'):
+                patterns = re.compile(validation_rules[var][v], re.I)
+                if(not re.fullmatch(patterns, dict[var])):
+                    err[v] = "Regex matching error occurs (Set patterns: {} obtained value: {})".format(validation_rules[var][v], dict[var])
+
+            elif(v == 'enumerate'):
+                if(dict[var] not in validation_rules[var][v]):
+                    err[v] = "Out of defined value (Set values: {}, obtained value: {})".format(validation_rules[var][v], dict[var])
+
+            else:
+                print("Validator not Found ")
+        if(err):
+            errors[var]=err
+    
+    if(errors): # If any Error
+        return False
+    else:
+        return True
+
+
+
+validation_rules={
+    'name':{
+        'type': 'string',
+        'minLength':10,
+        'maxLength': 500
+    },
+    'address':{
+        'type': 'integer',
+        'minLength': 100,
+        'maxLength':50,
+        'required':True,
+    },
+    'age':{
+        'type': 'integer',
+        'minValue': 5,
+        'maxValue':30,
+    },
+    'email':{
+        "regex" : "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
+    },
+    'day':{
+        'enumerate':['sunday', 'monday', 'tuesday', 'thrusday', 'friday', 'saturday']
+    }
+}
+
+
+dict ={'name': 'Bhuban Yadav', 'age':24, 'day':'noday' ,'birthYear':1998, 'birthMonth': 4, 'birthDay': 18, 'address': 'Dhapakhel-23, Lalitpur Nepal','email':'yadav.bhuban.by@gmail.com'}
+
+
+
+
+print(validator(dict, validation_rules))
+print("Errors: {}".format(errors))
